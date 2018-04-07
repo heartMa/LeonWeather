4afc27de444fa08f3b1a67108a3c904408220fa2
891091835 <891091835@qq.com>
2018-04-07 15:47:53 +0800

    test

--- app/src/main/java/net/heartma/leonweather/MainActivity.java
+++ app/src/main/java/net/heartma/leonweather/MainActivity.java
@@ -5,9 +5,10 @@ import android.content.SharedPreferences;
 import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
-
+    private static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +18,7 @@ public class MainActivity extends AppCompatActivity {
         if (prefs.getString("weather", null) != null) {
             Intent intent = new Intent(this, WeatherActivity.class);
             startActivity(intent);
+            Log.d(TAG, "onCreate: ");
             finish();
         }
     }
